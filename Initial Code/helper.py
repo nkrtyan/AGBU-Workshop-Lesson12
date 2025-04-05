ef5f2650cdae00d7c12ed8b22196ec3f672e8fcd
Syuzanna <khumaryansyuzi@gmail.com>
2025-04-05 12:40:41 +0400

    file is ready

--- Initial Code/helper.py
+++ Initial Code/helper.py
@@ -1,19 +1,30 @@
 import logging
+import os
+import pandas as pd
+import shutil
+
+
+
+
+
+
 class Helper:
     def __init__(self, dirname="Dataset", user_excel_name="users.xlsx", course_excel_name="courses.xlsx"):
         logging.basicConfig()
-        pass
+        
 
 
-    def create_dir(self):
+    def create_dir(self, dirname):
         """Create dirname=<Dataset> folder, return folder path"""
         pass
+        
     
     
     def create_and_write_users_excel(self, excel_data):
         """ Create excel "users.xlsx", in case file doesn't exists
         Excel sheet should contain username, email, password, account_balance, user_role, logged_in columns
         """
+
         pass
 
     def create_and_write_courses_excel(self, excel_data):
@@ -31,10 +42,31 @@ class Helper:
         """
         pass
 
-
+    
     def clean_up(self):
-        """Clean  all data(directory with excel files and log file) created during code execution """
+        confirm = input(f"Do you want to delete '{self.dirname}' and log file '{self.logname}'? (yes/no): ").strip().lower()
+
+        if confirm == "yes":
+            if os.path.exists(self.dirname):
+                shutil.rmtree(self.dirname)
+                print(f"Directory '{self.dirname}' deleted.")
+            else:
+                print(f"Directory '{self.dirname}' does not exist.")
+
+            if os.path.exists(self.logname):
+                print(f"Log file '{self.logname}' deleted.")
+            else:
+                print(f"Log file '{self.logname}' does not exist.")
+
+            logging.shutdown()
+            if os.path.exists(self.logname):
+                os.remove(self.logname)
+        else:
+            print("Files and the directory are not deleted.")
         # logging.shutdown(), before delete shutdown the logging file
-        pass
+
+
+
+     
 
 
