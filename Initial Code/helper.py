67f82a48e3d16d48c0406da69600d66380512e39
nkrtyan <nelly_krtyan@mail.ru>
2025-04-05 12:22:14 +0400

    Added team 1 work

--- Initial Code/helper.py
+++ Initial Code/helper.py
@@ -1,15 +1,34 @@
 import logging
+import os
+
 class Helper:
     def __init__(self, dirname="Dataset", user_excel_name="users.xlsx", course_excel_name="courses.xlsx"):
-        logging.basicConfig()
-        pass
+        self.dirname = dirname
+        self.user_excel_name = user_excel_name
+        self.course_excel_name = course_excel_name
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            filename="project.log",
+            filemode="a",
+            encoding="utf-8",
+        )
+
+        logging.info(f"The name of directory is {dirname}")
+        logging.info(f"The name of users excel is {user_excel_name}")
+        logging.info(f"The course excel name is {course_excel_name}")
 
 
     def create_dir(self):
-        """Create dirname=<Dataset> folder, return folder path"""
-        pass
+        if not os.path.exists(self.dirname):
+            os.mkdir(self.dirname)
+            logging.info(f"Directory {self.dirname} was created")
+        else:
+            logging.info(f"Directory {self.dirname} already exists")
+        return os.path.abspath(self.dirname)
     
-    
+
     def create_and_write_users_excel(self, excel_data):
         """ Create excel "users.xlsx", in case file doesn't exists
         Excel sheet should contain username, email, password, account_balance, user_role, logged_in columns
