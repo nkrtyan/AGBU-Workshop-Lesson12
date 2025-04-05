67300b1f96cfc8d0394268f0c6100415af51417e
nkrtyan <nelly_krtyan@mail.ru>
2025-04-05 12:38:43 +0400

    data is added

---
+++ data.py
@@ -0,0 +1,19 @@
+# Admin_User
+admin_user_role = "Admin"
+admin_username = "testuser123"
+admin_password = "SecurePass!2024" 
+admin_email = "testuser123@example.com"
+admin_account_balance = "1000"
+admin_validation_message = "User created successfully"
+# Non_admin_User
+non_admin_user_role = "Non Admin"
+non_admin_username = "testuser123"
+non_admin_password = "SecurePass!2024" 
+non_admin_email = "testuser123@example.com"
+non_admin_account_balance = "1000"
+non_admin_validation_message = "User created successfully"
+# Course_data
+course_title = "Automation_QA"
+course_price = "100USD"
+course_duration = "3 month"
+course_start_date = "15/04/2025"
