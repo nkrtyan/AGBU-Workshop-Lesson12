9f407342ea71f465b34784c026537b02858824f5
Anush <akopyan.anush@gmail.com>
2025-04-05 14:44:44 +0400

    Team4

--- Initial Code/project_structure/buy_course.py
+++ Initial Code/project_structure/buy_course.py
@@ -1,8 +1,28 @@
+import pandas as pd
+from helper import Helper
+import os
+import logging
+
 class Buy_Course(Helper):
-    def __init__(self, title, username):
+    def __init__(self, course_name, username):
         super().__init__()
-        pass
+        self.course_name = course_name
+        self.username = username
 
     def buy_course(self):
-        """Logged in user find course with given course_name and add in the same line course_buyser"""
-        pass
+        path = os.path.join("dataset", "courses.xlsx")
+        df = pd.read_excel(path)
+        for index, row in df.iterrows():
+            if row['title'] == self.course_name:
+                df.at[index, 'username'] = self.username
+                break
+        logging.info(f"{self.username} added to {self.course_name}")
+        df.to_excel(path, index=False)
+        
+
+data = Buy_Course("course3", "Da")
+data.buy_course()
+
+
+"""Logged in user find cours
+        e with given course_name and add in the same line course_buyser"""
